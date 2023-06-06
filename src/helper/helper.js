b0f0e970b69baa74006a77ade8d9bee3cf792e64
wyenelle <favourkcb@gmail.com>
2023-06-06 06:21:35 -0700

    fix: deployment error on helper.js

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,4 +1,4 @@
-import Woman from '../assets/unsplash/Woman.png'
+import Woman from '../assets/unsplash/woman.png'
 import WomanBrown from '../assets/unsplash/girl-brown-jacket.png'
 import WomanBlack from '../assets/unsplash/girl-black-jacket.png'
 
